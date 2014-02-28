dbeb03682bd56b87c491619ef5608b8dd845e1eb
YAMANE Toshiaki <yamanetoshi@gmail.com>
2014-02-28 14:45:32 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoiceMemoApi::Application.config.secret_token = '216111bb703e08d3014bec816b9a90d8f2a7f6734438133153681ae3bb59c66f2274e3ab26d27c455ff3ebbef1470b67152664ecd7f5ef6ebc237f47226c76da'
